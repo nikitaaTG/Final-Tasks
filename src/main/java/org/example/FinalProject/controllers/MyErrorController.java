43a8a996cdbf1c940c09e42c01020cff17590b0f
Nikita Tarasov-Gaevskii <nikinou@yandex.ru>
2023-03-27 15:35:54 +0200

    - Added: 1. Error handler controller & page - Small view & logic fixes -

---
+++ src/main/java/org/example/FinalProject/controllers/MyErrorController.java
@@ -0,0 +1,15 @@
+package org.example.FinalProject.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        //do something like logging
+        return "homepage/error";
+    }
+}
